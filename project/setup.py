f3bdc2c508fc7b77cd778be0e4f5a700a10b0255
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-11-29 15:10:44 +0000

    Basic of making a library

---
+++ project/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='automl',
+    packages=find_packages(include=['automl']),
+    version='0.1.0',
+    description='An AutoRL library',
+    author='Ricardo Gonçalo',
+)
