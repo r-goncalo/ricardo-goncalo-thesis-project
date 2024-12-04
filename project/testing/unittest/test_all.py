2a41025cd484d94971d77ae3674c239bb36a435c
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-12-04 01:29:42 +0000

    Added place for component value exposure tests

--- project/testing/unittest/test_all.py
+++ project/testing/unittest/test_all.py
@@ -1,6 +1,6 @@
 from project.testing.unittest.test_basic_components import *
 from project.testing.unittest.test_component_inheritence import *
-
+from project.testing.unittest.test_component_value_exposure import *
 
 if __name__ == '__main__':
     unittest.main()
---
+++ project/testing/unittest/test_component_value_exposure.py
@@ -0,0 +1,8 @@
+from ...automl.basic_components import loop_components
+from ...automl.component import Component, input_signature
+import unittest
+              
+        
+
+if __name__ == '__main__':
+    unittest.main()
