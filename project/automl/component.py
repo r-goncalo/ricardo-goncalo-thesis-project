97a618f745f2a6139238c3f556612e4b0c8c82a8
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-12-24 11:54:29 +0000

    Component: Changed system that verifies validity and added requires_input_proccess annotation
    @requires_input_proccess is a decorator supposed to be used on methods that need input proccessing before they are ran. When those are called before input proccessing is done, input proccessing will be automatically done. Note that adding input marks a component as not having its input proccessed.

--- project/automl/component.py
+++ project/automl/component.py
@@ -10,6 +10,9 @@ class Component: # a component that receives and verifies input
     # if default_value is None, an exception error is raised when input is missing
     # if validity verification function is not none, it will be applied to the input value
     # the actual input values will be saved in self.input
+    # worth noting that child components can freely repeat values in their input_signatures.
+    #   Default values and generators of child components will have priority
+    #   Validity and type checkers will be applied from Parent to child     
     input_signature = {}
     
     #A dictionary { "value_name" -> initial_value }
@@ -29,9 +32,17 @@ class Component: # a component that receives and verifies input
         self.pass_input(input) #passes the input but note that it does not proccess it
         
         self.output = {} #output, if any, will be a dictionary
+        
+        self._input_was_proccessed = False #to track if the instance has had its input proccessing before any operations that needed it
             
     
     def pass_input(self, input: dict): # pass input to this component
+        '''Pass input to this component
+           It is supposed to be called only before proccess_input and, if not, it mean proccess_input may be called a multitude of times
+           '''
+        
+        self._input_was_proccessed = False #when we pass new input, it means that we need to proccess it again
+        
         for passed_key in input.keys():
             self.input[passed_key] = input[passed_key]
 
@@ -39,6 +50,7 @@ class Component: # a component that receives and verifies input
         '''Verify validity of input and add default values''' 
         self.__add_default_values_of_class_and_super(type(self))
         self.__proccess_input_of_class_and_super(type(self))
+        self._input_was_proccessed = True
 
 
     # OUTPUT -------------------------
@@ -49,6 +61,7 @@ class Component: # a component that receives and verifies input
 
     # EXPOSED VALUES -------------------------------------------
     
+    # TODO : maybe this should happen at the end of class definition for all classes that extend Component, statically? Instead of happening multiple times per class redundantly
     def __set_exposed_values_with_super(self, class_component):
         
         '''Updates the exposed values with super classes'''
@@ -83,11 +96,11 @@ class Component: # a component that receives and verifies input
                 
                 input_value = self.input[input_key] #get the value passed
                     
-                verify_validity(input_key, input_value, possible_types, validity_verificator) #raises exceptions if input is not valid
+                self.verify_validity(input_key, input_value, possible_types, validity_verificator) #raises exceptions if input is not valid
                                     
             else: #if there was no specified value for this attribute in the input
                 
-                raise Exception(f"Did not set input for value  with key '{input_key}' and has no default value nor generator")     
+                raise Exception(f"In component of type {type(self)}: Did not set input for value  with key '{input_key}' and has no default value nor generator")     
                 
     # DEFAULT VALUES -------------------------------------------------
     
@@ -115,47 +128,54 @@ class Component: # a component that receives and verifies input
                     self.input[input_key] = default_value  #the value used will be the default value
 
                 elif not generator == None:
-                    self.input[input_key] = generator()              
+                    self.input[input_key] = generator(self) #generators have access to the instance              
                 
+
+    def verify_validity(self, input_key, input_value, possible_types, validity_verificator):
+
+        if validity_verificator == None: #if there was no specified validity_verification, we check the available types
+            return self.verify_one_of_types(input_key, input_value, possible_types) 
+
+        is_a_correct_type = validity_verificator(input_value) #use the verificator the Component has specified in its input signature
+
+        if not isinstance(is_a_correct_type, bool): #validity_verification must return bool
+            raise Exception(f"In component of type {type(self)}: Validity verification on key '{input_key}' returned a type other than bool")
+
+        elif is_a_correct_type == False:
+            raise Exception(f"In component of type {type(self)}: Value with key '{input_key}' did not pass Component specified validity verificator") 
+
+
+
+
+    def verify_one_of_types(self, input_key, input_value, possible_types):
+
+            if len(possible_types) == 0:
+                return #if there were no possible_types defined, there is no need to check the type
+
+            for possible_type in possible_types:
+
+                if isinstance(input_value, possible_type):
+                    return #break the loop and the functon, value is of one of the possible types
+
+            #if we reach the end of the function, then the value is of none of the types
+            raise Exception(f"No validity verificator specified for key '{input_key}' and its type is of none of the possible types: {possible_types}")
           
 
 # VALIDITY VERIFICATION (static methods for validating input) -----------------------------          
+
+def requires_input_proccess(func):
+    '''An annotation that makes the input be proccessed, if it was not already, when a function is called'''
+    def wrapper(self : Component, *args, **kwargs):
+        if not self._input_was_proccessed:
+            self.proccess_input()
+        return func(self, *args, **kwargs)
+    return wrapper
+
                     
 #a function that generates a single input signature
 def input_signature(default_value=None, generator=None, validity_verificator=None, possible_types : list = []):
 
     return  (default_value, generator, possible_types, validity_verificator)
-
-
-
-def verify_validity(input_key, input_value, possible_types, validity_verificator):
-    
-    if validity_verificator == None: #if there was no specified validity_verification, we check the available types
-        return verify_one_of_types(input_key, input_value, possible_types) 
-    
-    is_a_correct_type = validity_verificator(input_value) #use the verificator the Component has specified in its input signature
-    
-    if not isinstance(is_a_correct_type, bool): #validity_verification must return bool
-        raise Exception(f"Validity verification on key '{input_key}' returned a type other than bool")
-    
-    elif is_a_correct_type == False:
-        raise Exception(f"Value with key '{input_key}' did not pass Component specified validity verificator") 
-
-
-
-
-def verify_one_of_types(input_key, input_value, possible_types):
-        
-        if len(possible_types) == 0:
-            return #if there were no possible_types defined, there is no need to check the type
-        
-        for possible_type in possible_types:
-            
-            if isinstance(input_value, possible_type):
-                return #break the loop and the functon, value is of one of the possible types
-            
-        #if we reach the end of the function, then the value is of none of the types
-        raise Exception(f"No validity verificator specified for key '{input_key}' and its type is of none of the possible types: {possible_types}")
 
 
 
