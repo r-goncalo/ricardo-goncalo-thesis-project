c17cce41ac17720e46ceb0651fe82ebc73dfd1e0
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2025-06-11 22:35:54 +0100

    Update setup.py

--- project/setup.py
+++ project/setup.py
@@ -6,5 +6,19 @@ setup(
     version='0.1.0',
     description='An AutoRL library',
     author='Ricardo Gonçalo',
-    install_requires = []
+    install_requires = [
+        'numpy',
+        'optuna',
+        'matplotlib',
+        'torch',
+        'gymnasium',
+        'pandas',
+        'wandb',
+        'pettingzoo',
+        'pygame',
+        'scikit-learn',
+        'plotly',
+        'nbformat'
+        
+    ]
 )
