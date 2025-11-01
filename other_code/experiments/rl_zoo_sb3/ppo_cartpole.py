a2ea98703591f2d82c74f47f2f67081e5f98ac7a
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2025-11-01 18:52:23 +0000

    Update ppo_cartpole.py

--- other_code/experiments/rl_zoo_sb3/ppo_cartpole.py
+++ other_code/experiments/rl_zoo_sb3/ppo_cartpole.py
@@ -4,10 +4,12 @@
 from experiments import hp_experiments_sequence
 
 
-def experiment_1(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
+def experiment_for_poo_actors(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
                  base_to_opt_config_path="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\configurations\\to_optimize_configuration.json",
                  hp_opt_config_path="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\configurations\\configuration_3.json", 
+                 mantain_original=False,
                  directory_of_models="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\models",
+                 models_to_test=None,
                  experiment_name="sb3_zoo_dqn_cartpole_hp_opt_mult_samplers_pruners",
                  base_commands=None):
 
@@ -53,18 +55,24 @@ def experiment_1(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
     expanded_commands_for_models = hp_experiments_sequence.expand_commands_for_each_model(
         command_dicts=base_commands,
         directory_of_models=directory_of_models,
-        directory_to_store_definitions=directory_to_store_definitions
+        directory_to_store_definitions=directory_to_store_definitions,
+        mantain_original=mantain_original,
+        models_to_test=models_to_test
     )
 
     hp_experiments_sequence.guarantee_same_path_in_commands(expanded_commands_for_models)
 
-    return expanded_commands_strs_for_models
+    return expanded_commands_for_models
 
 
-def experiment_2(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
+def experiment_for_poo_actors_and_critics(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
                  base_to_opt_config_path="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\configurations\\to_optimize_configuration.json",
                  hp_opt_config_path="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\configurations\\configuration_3.json", 
+                 mantain_original=False,
                  directory_of_models="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\models",
+                 models_to_test=None,
+                 directory_of_critics="C:\\rgoncalo\\experiment_definitions\\dqn_cartpole_sb3_zoo\\models",
+                 critics_to_test=None,
                  experiment_name="sb3_zoo_dqn_cartpole_hp_opt_mult_samplers_pruners",
                  base_commands=None):
 
@@ -110,11 +118,19 @@ def experiment_2(directory_to_store_experiment = "C:\\rgoncalo\\experiments",
     expanded_commands_for_models = hp_experiments_sequence.expand_commands_for_each_model(
         command_dicts=base_commands,
         directory_of_models=directory_of_models,
-        directory_to_store_definitions=directory_to_store_definitions
+        directory_to_store_definitions=directory_to_store_definitions,
+        mantain_original=mantain_original,
+        models_to_test=models_to_test
     )
 
-    hp_experiments_sequence.guarantee_same_path_in_commands(expanded_commands_for_models)
+    expanded_commands_strs_for_critics = hp_experiments_sequence.expand_commands_for_each_critic_model(
+        command_dicts=expanded_commands_for_models,
+        directory_of_models=directory_of_critics,
+        directory_to_store_definitions=directory_to_store_definitions,
+        models_to_test=critics_to_test,
+        mantain_original=mantain_original
+    )
 
-    expanded_commands_strs_for_models = hp_experiments_sequence.make_command_dicts_command_strings(expanded_commands_for_models)
+    hp_experiments_sequence.guarantee_same_path_in_commands(expanded_commands_strs_for_critics)
 
-    return expanded_commands_strs_for_models
+    return expanded_commands_strs_for_critics
