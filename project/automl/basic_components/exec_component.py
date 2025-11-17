f45dff21435990dd4db9ab0a9e0dcc441fdf95a0
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2025-11-17 11:05:22 +0000

    Update exec_component.py

--- project/automl/basic_components/exec_component.py
+++ project/automl/basic_components/exec_component.py
@@ -2,6 +2,7 @@ from automl.loggers.component_with_results import save_all_dataframes_of_compone
 from automl.basic_components.state_management import save_state
 from automl.utils.smart_enum import SmartEnum
 from automl.loggers.logger_component import flush_text_of_all_loggers_and_children
+from project.automl.loggers.global_logger import globalWriteLine
 from ..component import InputSignature, Component, requires_input_proccess
 
 from abc import abstractmethod
@@ -73,6 +74,9 @@ class ExecComponent(Component):
 
     def __on_exception(self, exception):
         '''To be called when a non treated exception happens'''
+
+        globalWriteLine(f"Component {self.name} of type {type(self)} is dealing with the exception: {exception}")
+
         self._deal_with_exception(exception)
     
     def _deal_with_exception(self, exception):
