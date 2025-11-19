632aa011c84eb646fd0cc86f32cb6e6565d2c808
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2025-11-19 10:39:08 +0000

    Create paths.py

---
+++ other_code/paths.py
@@ -0,0 +1,8 @@
+
+
+
+
+SCRIPT_PATH = "C:\\ricardo-goncalo-thesis-project\\other_code\\RunSimplyHpExpCommand.bat"
+HP_SCRIPT_PATH = "C:\\ricardo-goncalo-thesis-project\\project\\examples\\simple_metarl\\scripts\\run_hp_experiment.py"
+
+
