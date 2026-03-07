9d92a9baa9c394c826248be3f7e8520fc613cda2
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2026-03-07 12:59:10 +0000

    Update _version.py

--- project/automl/_version.py
+++ project/automl/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.1.dev4+g78fb06e76.d20260306'
-__version_tuple__ = version_tuple = (1, 0, 1, 'dev4', 'g78fb06e76.d20260306')
+__version__ = version = '1.0.1.dev6+g5f7e3df3e.d20260307'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev6', 'g5f7e3df3e.d20260307')
 
-__commit_id__ = commit_id = 'g78fb06e76'
+__commit_id__ = commit_id = 'g5f7e3df3e'
