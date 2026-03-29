4b4e959c7a36ca6c11bdfec6dbc4554bd17c7b97
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2026-03-30 00:43:24 +0100

    Update _version.py

--- project/automl/_version.py
+++ project/automl/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.0.1.dev108+gfafd74135.d20260329'
-__version_tuple__ = version_tuple = (1, 0, 1, 'dev108', 'gfafd74135.d20260329')
+__version__ = version = '1.0.1.dev110+g5f5024fbf.d20260329'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev110', 'g5f5024fbf.d20260329')
 
-__commit_id__ = commit_id = 'gfafd74135'
+__commit_id__ = commit_id = 'g5f5024fbf'
